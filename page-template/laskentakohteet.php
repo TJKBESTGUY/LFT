eece07e59248ff42473ce07ebc800972ffc350be
TJGOODMAN <>
2022-02-23 10:41:49 +0200

    add button

--- page-template/laskentakohteet.php
+++ page-template/laskentakohteet.php
@@ -859,12 +859,12 @@ $posts_laskenta = get_posts(array(
               <h3>Kiinnostuitko jostain kohteesta?<br>Pyydä tarjous lomakkeen kautta tai ottamalla yhteyttä alueen yhteyshenkilöön.</h3>
                 </div>
                 <div class="capsule-wrap">
-                <button class="basic-btn btn--black" type="button" name="button"><span class="basic-btn__text">Pyydä tarjous</span> <span class="basic-btn__icon">
+                <button class="basic-btn btn--black js--show-form-modal" type="button" name="button"><span class="basic-btn__text">Pyydä tarjous</span> <span class="basic-btn__icon">
                   <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18"><path d="M9,.5A8.5,8.5,0,1,0,17.5,9,8.51,8.51,0,0,0,9,.5Zm1.08,12.93-.91-.94L12,9.7H3.83V8.32H12L9.17,5.51l.91-.94L14.48,9Z"></path></svg>
                 </span> </button>
-                <button class="basic-btn btn--yellow" type="button" name="button"><span class="basic-btn__text">Näytä yhteyshenkilöt</span> <span class="basic-btn__icon">
+                <!-- <button class="basic-btn btn--yellow" type="button" name="button"><span class="basic-btn__text">Näytä yhteyshenkilöt</span> <span class="basic-btn__icon">
                   <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18"><path d="M9,.5A8.5,8.5,0,1,0,17.5,9,8.51,8.51,0,0,0,9,.5Zm1.08,12.93-.91-.94L12,9.7H3.83V8.32H12L9.17,5.51l.91-.94L14.48,9Z"></path></svg>
-                </span> </button>
+                </span> </button> -->
               </div>
             </div>
           </div>
