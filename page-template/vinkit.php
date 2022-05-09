645dc721b36a424d44010a40bfa8c624e9f97b54
TJGOODMAN <>
2022-05-09 16:07:44 +0300

    clean up

--- page-template/vinkit.php
+++ page-template/vinkit.php
@@ -58,72 +58,9 @@
 <?php endif; ?>
 
 
-    <div class="module--content-block--vinkit">
-
-        <div class="vinkit__heading">
-      <h2>Kuinka pukeudun ja mitä otan mukaan?</h2>
-        </div>
-
-    <div class="vinkit__txt-content">
-
-        <p>Pukeudu sään mukaan riittävän lämpimästi. Tarkista sääennuste ennen retkelle lähtöä ja ole valmistautunut nopeisiin ja yllättäviin sään muutoksiin tunturialueilla ja erämaissa liikkuessasi. Ei ole tavatonta, että olosuhteet muuttuu yhtäkkiä aurinkoisesta tuuliseksi ja sateiseksi. Lisäksi nopea lämpötilan viileneminen ja satunnaiset raekuurot ovat osa Lapin kesän sääilmiöitä. Siksi onkin hyvä pakata mukaan tuulelta ja sateelta suojaava vaatekerta. Pyöräilyhanskat ovat myös hyvä pitää matkassa ja pyöräilykypärä ehdoton. Mikäli sinulla ei ole kaikkia tarvittavia varusteita, löydät liikkeestämme mm. laadukkaita pyöräilyhanskoja. Kypärä kuuluu mukaan pyörän vuokraukseen.
-
-Ota lisäksi mukaan kartta, juomapullo, evästä. Liikkeestämme voit ostaa juomapullon sekä eväspatukoita retken aikaiseen tarpeeseen. Myös ensiaputarvikkeet on hyvä olla matkassa.
-
-Lisäksi pysähtyminen paikallisissa kahviloissa ja ravintoloissa on suositeltavaa. Ulkoilu kuluttaa runsaasti energiaa ja paikalliset yritykset palvelevat mielellään pyöräileviä asiakkaita.</p>
-    </div>
-  </div>
-
-  <div class="module--content-block--vinkit">
-
-      <div class="vinkit__heading">
-    <h2>Puhelin mukaan!</h2>
-      </div>
-
-  <div class="vinkit__txt-content">
-<p>Hätänumero on 112.
-
-Hätätilanteessa 112 Suomi –mobiilisovelluksen avulla sinut voidaan paikantaa nopeammin.
-Lataa puhelimen akku täyteen ennen retkeä ja huolehdi, ettei puhelin pääse kastumaan.
-Osa reiteistä on puhelimen katvealueella. Puhelimen kuuluvuus on parempi korkeilla paikoilla kuin laaksoissa ja kuruissa.
-
-Jätä pidemmistä reiteistä suunnitelma vuokraamoon.</p>
-  </div>
-</div>
 
 
-<div class="module--content-block--vinkit">
 
-    <div class="vinkit__heading">
-  <h2>Muista etiketti</h2>
-    </div>
-
-<div class="vinkit__txt-content">
-
-<ul>
-  <li>
-    Ota huomioon muut reiteillä kulkijat. Pyöräilijä nopeampana on valmis väistämään muita reiteillä liikkujia, kuten kävelijöitä ja vaeltajia.
-  </li>
-  <li>
-    Huomioi oikea tilannenopeus etenkin kohtaamisissa, mutta myös pitkissä alamäissä.
-  </li>
-  <li>
-    Ennen kohtaamisia ilmoita tulostasi moikkaamalla ystävällisesti.
-  </li>
-  <li>
-    Oikea ajotapa ei kuluta maastoa tai vahingoita polkua. Pysy aina polulla äläkä hae helpompaa ajoalustaa kiertämällä.
-  </li>
-
-</ul>
-<p>Hätänumero on 112.
-
-Hätätilanteessa 112 Suomi –mobiilisovelluksen avulla sinut voidaan paikantaa nopeammin.
-Lataa puhelimen akku täyteen ennen retkeä ja huolehdi, ettei puhelin pääse kastumaan.
-Osa reiteistä on puhelimen katvealueella. Puhelimen kuuluvuus on parempi korkeilla paikoilla kuin laaksoissa ja kuruissa.
-
-Jätä pidemmistä reiteistä suunnitelma vuokraamoon.</p>
-</div>
-</div>
 
     </div>
           </section>
