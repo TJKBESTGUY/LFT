89b7f5d11328dc631bd3e8f4194bf3935ba89d25
TJGOODMAN <>
2023-03-29 11:22:29 +0300

    push referenssit

--- header.php
+++ header.php
@@ -374,7 +374,7 @@ tuottavuuden nostamiseen keskittyviä henkilöstön kehittämisen palveluita tie
                   </div>
 
 
-                    <a class="-nav-link Target-- -main-link" href="/asiakastarinat/"> <span>Asiakastarinat</span></a>
+                    <a class="-nav-link Target-- -main-link" href="/kokemuksia/"> <span>Referenssit</span></a>
 
                   <div class="dropdown-nav dropdown-nav--ajankohtaista" x-data="{ dropdown_open: false }" :class="dropdown_open && 'S-active--dropdown'"  @mouseleave="dropdown_open = false">
                       <button class="-nav-link" href="/" @mouseenter="dropdown_open = true; $store.dropdown = true" @keydown.enter="dropdown_open = ! dropdown_open; $store.dropdown = true">Ajankohtaista</button>
