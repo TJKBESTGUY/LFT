16f05bb3b3501167b655c37c7e79e0a3f5c0c07d
TJGOODMAN <>
2023-06-15 14:19:26 +0300

    use extended image

--- functions.php
+++ functions.php
@@ -696,7 +696,7 @@ if ( ! function_exists( 'liftcast_loop_output' ) ) :
 
 				             <div class="lazy-img-aspect">
 
-				             	<img class="lazyload lazyanim" data-src="<?php echo get_template_directory_uri(); ?>/images/lifcast_cropped.jpg" alt="Lifcast">
+				             	<img class="lazyload lazyanim" data-src="<?php echo get_template_directory_uri(); ?>/images/lifcast_extended.jpg" alt="Lifcast">
 				      </div>
 				      </div>
 
