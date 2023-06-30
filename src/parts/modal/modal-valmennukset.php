ecdc1861223baadef068d29ed6f79b2a0b48eed7
TJGOODMAN <>
2023-06-30 15:02:08 +0300

    text change

--- src/parts/modal/modal-valmennukset.php
+++ src/parts/modal/modal-valmennukset.php
@@ -98,7 +98,7 @@
                                 <button @click="hideModal" class="btn--basic btn--outline" style=" " href="">Sulje ikkuna</button>
                               </div>
                               <div class="static-luento-page-cta" style="display:none">
-                                <h4>Tutustu luentoihimme ja varaa.</h4>
+                                <h4>Tutustu luentoihimme:</h4>
                               <div class="capsule-wrap capsule-wrap--center">
                                 <a href="/luennot/" class="btn--basic btn--outline" style=" " href="">Kaikki luennot</a>
                               </div>
