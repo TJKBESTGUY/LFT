0b80040f34beb8a28652c660f4ce06b3b20b2c13
TJGOODMAN <tatu@gdmn.fi>
2023-10-10 11:08:12 +0300

    link corrected

--- src/parts/modal/modal-valmennukset-en.php
+++ src/parts/modal/modal-valmennukset-en.php
@@ -107,7 +107,7 @@
                               <div class="static-luento-page-cta" style="display:none">
 
                               <div class="capsule-wrap capsule-wrap--center">
-                                <a  class="btn--basic btn--outline" style=" " href="https://lifted.fi/lectures">Lectures & workshops</a>
+                                <a  class="btn--basic btn--outline" style=" " href="https://lifted.fi/en/lectures-workshops/">Lectures & workshops</a>
                               </div>
                                       </div>
 
