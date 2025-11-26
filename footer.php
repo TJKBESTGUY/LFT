af6c78f4260e9b2364cb891ed77530b9cdcefb3c
GoodmanWorldWide <tatu@gdmn.fi>
2025-11-26 12:30:12 +0200

    footer php

--- footer.php
+++ footer.php
@@ -120,7 +120,7 @@
 
 
 
-                 <?php while( have_rows('navigation_group_footer', 'option') ) : the_row(); ?>
+                 <?php while( have_rows('navigation_group_footer') ) : the_row(); ?>
 
 
 
@@ -346,7 +346,7 @@
 <!-- fixed STUFF ENDS-->
 
 <?php wp_footer(); ?>
-
+<script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.5.0/axios.min.js" integrity="sha512-aoTNnqZcT8B4AmeCFmiSnDlc4Nj/KPaZyB5G7JnOnUEkdNpCZs1LCankiYi01sLTyWy+m2P+W4XM+BuQ3Q4/Dg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
 
 
