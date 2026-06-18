82ace54e7eeb44ffa88c3305df6ea83c32f6ca56
GoodmanWorldWide <tatu@gdmn.fi>
2026-06-18 07:12:38 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -710,7 +710,7 @@ if ( ! function_exists( 'liftcast_loop_output' ) ) :
 
 				             <div class="lazy-img-aspect">
 
-				             	<img class="lazyload lazyanim" data-src="<?php echo get_template_directory_uri(); ?>/images/lifcast_extended.jpg" alt="Liftcast">
+				             	<img class="lazyload lazyanim" data-src="https://lifted.fi/wp-content/uploads/2026/06/Liftcast-UUSI-kansikuva-1.png" alt="Liftcast">
 				      </div>
 				      </div>
 
